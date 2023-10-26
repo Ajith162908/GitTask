0d73e02cb411c79cdeddc61cc3c63c9cf4079438
Ajith162908 <ak08092022@gmail.com>
2023-10-26 09:27:54 +0530

    Method create

---
+++ src/test/java/org/mm/Class1.java
@@ -0,0 +1,18 @@
+package org.mm;
+
+public class Class1 {
+	private void theni() {
+		System.out.println("theni");
+
+	}
+	private void chennai() {
+		System.out.println("chennai");
+
+	}
+	private void madurai() {
+		System.out.println("madurai");
+
+	}
+	
+
+}
